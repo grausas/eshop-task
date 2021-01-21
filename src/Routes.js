5133f0c63ea01a50f29a85b21d3468a16b753069
Marius <grausas@gmail.com>
2021-01-21 09:16:59 +0200

    clean up

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Header, Loading } from "./components";
+
+const HomeLazy = lazy(() => import("./pages/Home/Home"));
+
+function Routes() {
+  return (
+    <Router>
+      <Header />
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/" component={HomeLazy} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default Routes;
