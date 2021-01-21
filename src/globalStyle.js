3818f6444190bd9df303088ca93344ba028b6553
Marius <grausas@gmail.com>
2021-01-21 09:30:10 +0200

    global styles added

---
+++ src/globalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+   * {
+      background: #f8f4f1;
+      font-family: 'Open Sans', sans-serif;
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+   }
+`;
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./globalStyle";
 import theme from "./theme";
 import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Routes />
     </ThemeProvider>
   </React.StrictMode>,
